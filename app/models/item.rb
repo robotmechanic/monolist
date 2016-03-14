240ea849d33f18ba9f38855065b0b8d874992ba1
David H <dhovanky@gmail.com>
2016-03-14 13:42:16 +0000

    implemented user and item pages

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,10 +4,10 @@ class Item < ActiveRecord::Base
   has_many :ownerships  , foreign_key: "item_id" , dependent: :destroy
   has_many :users , through: :ownerships
   
-  has_many :want_users, class_name: "User", foreign_key: "item_id", dependent: :destroy
-  has_many :wants, through: :want_users, source: :user
+  has_many :wants, class_name: "Want", foreign_key: "item_id", dependent: :destroy
+  has_many :want_users, through: :wants, source: :user
   
-  has_many :have_users, class_name: "User", foreign_key: "item_id", dependent: :destroy
-  has_many :have, through: :have_users, source: :user
+  has_many :haves, class_name: "Have", foreign_key: "item_id", dependent: :destroy
+  has_many :have_users, through: :haves, source: :user
   
 end
